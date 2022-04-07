8b5a6147975f1b32d47dff95170bc5ad89b9f7e9
Jeferson Lima <jefersonsilvadelima15@gmail.com>
2022-04-06 23:38:46 -0300

    Continuidade dos recursos financeiros

--- src/index.js
+++ src/index.js
@@ -9,37 +9,97 @@ app.use(express.json());
 
 const customers = [];
 
+function verifyIfExistAccountCPF(request, response, next) {
+  const { cpf } = request.headers;
+
+  const customer = customers.find(customer => customer.cpf === cpf);
+    
+  if(!customer) {
+    return response.status(400).json({ error: "Customer not found!"});
+  }
+
+  request.customer = customer;
+
+  return next();
+}
+
+function getBalance(statement) {
+  const balance = statement.reduce((acc, operation) => {
+    if(operation.type === 'credit') {
+      return acc + operation.amount;
+      }else {
+        return acc - operation.amount;
+      }
+  }, 0);
+  
+  return balance;
+}
+
 app.post("/account", (request, response) => {
-    const { cpf, name } = request.body;
+  const { cpf, name } = request.body;
 
-    const customerAlreadyExists = customers.some(
-        (customer) => customer.cpf === cpf
-    );
+  const customerAlreadyExists = customers.some(
+    (customer) => customer.cpf === cpf
+  );
 
-    if(customerAlreadyExists) {
-        return response.status(400).json({ error: "Customer already exists!"});
-    }
+  if(customerAlreadyExists) {
+    return response.status(400).json({ error: "Customer already exists!"});
+  }
 
-    customers.push({
-        cpf,
-        name,
-        id: uuidv4(),
-        statement: []
-    });
+  customers.push({
+    cpf,
+    name,
+    id: uuidv4(),
+    statement: []
+  });
 
-    return response.status(201).send();
+  return response.status(201).send();
 });
 
-app.get("/statement", (request, response) => {
-    const { cpf } = request.headers;
+//app.use(verifyIfExistAccountCPF);
 
-    const customer = customers.find(customer => customer.cpf === cpf);
+app.get("/statement", verifyIfExistAccountCPF, (request, response) => {
+  const { customer } = request;
 
-    if(!customer) {
-        return response.status(400).json({ error: "Customer not found!"});
-    };
+  return response.json(customer.statement);
+});
 
-    return response.json(customer.statement);
-})
+app.post("/deposit", verifyIfExistAccountCPF, (request, response) => {
+  const { description, amount } = request.body;
+
+  const { customer } = request;
+
+  const statementOperation = {
+    description,
+    amount,
+    created_at: new Date(),
+    type: "credit"
+  }
+
+  customer.statement.push(statementOperation);
+
+  return response.status(201).send();
+});
+
+app.post("/withdraw", verifyIfExistAccountCPF, (request, response) => {
+  const { amount } = request.body;
+  const { customer } = request;
+
+  const balance = getBalance(customer.statement);
+
+  if(balance < amount) {
+    return response.status(400).json({error: "Insufficient funds!"})
+  }
+
+  const statementOperation = {
+    amount,
+    created_at: new Date(),
+    type: "debit",  
+  };
+
+  customer.statement.push(statementOperation);
+
+  return response.status(201).send();
+});
 
 app.listen(3333);
